723f29082997187a0f79e606f6ad2534d50fdb6f
Domagoj Petelinc <petkas55@users.noreply.github.com>
2022-11-14 19:27:14 +0100

    Initial commit

---
+++ signup.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>NewsPortal</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800&amp;display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic&amp;display=swap">
+    <link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
+    <link rel="stylesheet" href="assets/css/styles.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.css">
+</head>
+
+<body>
+    <header class="masthead" style="background-image: url('assets/img/login-signup-bg.jpg');">
+        <div class="overlay"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto position-relative">
+                    <div class="post-heading" style="height: 361px;">
+                        <h1 class="text-center"><br>Sign up&nbsp;</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <section class="position-relative py-4 py-xl-5">
+                        <div class="container">
+                            <div class="row d-flex justify-content-center">
+                                <div class="col-md-6 col-xl-4">
+                                    <div class="card mb-5" style="background: rgba(255,255,255,0);">
+                                        <div class="card-body d-flex flex-column align-items-center">
+                                            <form class="text-center" method="post">
+                                                <div class="mb-3"><input class="form-control" type="email" name="name" placeholder="Name"></div>
+                                                <div class="mb-3"><input class="form-control" type="email" name="surname" placeholder="Surname"></div>
+                                                <div class="mb-3"><input class="form-control" type="email" name="email" placeholder="Email"></div>
+                                                <div class="mb-3"><input class="form-control" type="email" name="username" placeholder="Username"></div>
+                                                <div class="mb-3"><input class="form-control" type="password" name="password" placeholder="Password"></div>
+                                                <div class="mb-3"><label class="form-label" style="color: rgb(255,255,255);">Please select your contry</label>
+                                                    <div class="dropdown"><button class="btn btn-primary dropdown-toggle" aria-expanded="false" data-bs-toggle="dropdown" type="button" style="background: #252424;border-color: var(--bs-card-cap-bg);border-radius: 15px;">Dropdown </button>
+                                                        <div class="dropdown-menu"><a class="dropdown-item" href="#">First Item</a><a class="dropdown-item" href="#">Second Item</a><a class="dropdown-item" href="#">Third Item</a></div>
+                                                    </div>
+                                                </div>
+                                                <div class="mb-3"><button class="btn btn-primary d-block w-100" type="submit" style="background: #252424;border-color: var(--bs-card-cap-bg);border-radius: 15px;">Sign up</button></div>
+                                            </form>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </div>
+        </div>
+    </header>
+    <footer style="background: #252424;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-10 col-lg-8 mx-auto">
+                    <ul class="list-inline text-center">
+                        <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-twitter fa-stack-1x fa-inverse" style="background: #1b1818;border-radius: 35px;"></i></span></li>
+                        <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-facebook fa-stack-1x fa-inverse" style="border-radius: 35px;background: #1b1818;"></i></span></li>
+                        <li class="list-inline-item"><span class="fa-stack fa-lg"><i class="fa fa-circle fa-stack-2x"></i><i class="fa fa-github fa-stack-1x fa-inverse" style="background: #1b1818;border-radius: 35px;"></i></span></li>
+                    </ul>
+                    <p class="text-muted copyright">Copyright&nbsp;©&nbsp;Domagoj Petelinc 2022</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.11.1/baguetteBox.min.js"></script>
+    <script src="assets/js/script.min.js"></script>
+</body>
+
+</html>
